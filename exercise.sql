40e4dd80cfec599c801cd53b14ab547e6cc815b4
Stijn van_der_Made <2120804@talnet.nl>
2020-01-23 10:48:17 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE Sterrenstelsel;
+-- USE sterrenstelsel;
+-- CREATE TABLE Planeten (name VARCHAR(20));
+-- INSERT INTO planeten (name) VALUES('Mercurius');
+-- INSERT INTO planeten (name) VALUES('Aarde');
+-- INSERT INTO planeten (name) VALUES('Mars');
+-- SHOW TABLES;
+-- describe planeten;
+
+
